5ce4eb75c0085d39ec6a4edd5e9f8548338eb21f
jkim68888 <jkim68888@gmail.com>
2024-07-20 19:08:49 +0900

    xcode project 생성, app policy 업로드

---
+++ modoosSubway/modoosSubway/modoosSubwayApp.swift
@@ -0,0 +1,32 @@
+//
+//  modoosSubwayApp.swift
+//  modoosSubway
+//
+//  Created by 김지현 on 2024/07/20.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct modoosSubwayApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
