b8f4596279d2e54e777cab3e384137a691c50662
LimJaeHyeon9298 <limjh9298@gmail.com>
2024-11-15 17:13:29 +0900

    [feat] ModifyView UI 작업 진행 중

--- modoosSubway/modoosSubway/Presentation/View/ModifyFolderView.swift
+++ modoosSubway/modoosSubway/Presentation/View/ModifyFolderView.swift
@@ -17,7 +17,7 @@ struct ModifyFolderView: View {
                     dismiss()
                 }) {
                     Image(.back)
-                    Text("폴더명 수정하기")
+                    Text("폴더 수정하기")
                         .padding(.leading, -10)
                         .tint(._333333)
                 }
@@ -50,3 +50,203 @@ struct ModifyFolderView: View {
 
     }
 }
+
+struct ModifyView: View {
+    @State private var folderTextFieldString: String = ""
+    @State private var descriptionTextFieldString: String = ""
+    
+    var body: some View {
+        VStack {
+            VStack(alignment: .leading) {
+                Text("폴더명")
+                    .font(.pretendard(size: 14, family: .medium))
+                    .foregroundStyle(.black)
+                    .padding(.leading,24)
+                TextFieldView(text: $folderTextFieldString, placeholder: "폴더명을 입력해 주세요")
+            }
+            
+            VStack(alignment: .leading) {
+                Text("설명")
+                    .font(.pretendard(size: 14, family: .medium))
+                    .foregroundStyle(.black)
+                    .padding(.leading,24)
+                TextFieldView(text: $descriptionTextFieldString, placeholder: "설명을 입력해 주세요")
+                
+            }
+            
+            VStack {
+                Text("사진")
+                    .font(.pretendard(size: 14, family: .medium))
+                    .foregroundStyle(.black)
+                    .padding(.leading,24)
+                
+                
+            }
+  
+            
+            
+        }
+        .toolbar {
+            ToolbarItem(placement: .keyboard) {
+                HStack {
+                    Spacer()
+                
+                    Button(action: {
+                              hideKeyboard()
+                    }) {
+                        Image(systemName: "keyboard.chevron.compact.down")
+                            .foregroundColor(.blue)
+                    }
+                }
+            }
+        }
+    }
+    
+    private func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+
+struct TextFieldView: View {
+    @Binding var text: String
+    var placeholder: String
+    
+    init(text: Binding<String>, placeholder: String) {
+        self._text = text
+        self.placeholder = placeholder
+    }
+    
+    var body: some View {
+        HStack {
+            TextField(placeholder, text: self.$text)
+                .frame(height: 56)
+                .textFieldStyle(.plain)
+                .padding([.horizontal], 4)
+                .cornerRadius(16)
+                .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.gray))
+                .padding([.horizontal], 4)
+                .submitLabel(.search)
+                .onSubmit {
+                    //   handleSearch()
+                }
+                .onAppear {
+                    UITextField.appearance().clearButtonMode = .whileEditing
+                }
+                .autocorrectionDisabled()
+                .textInputAutocapitalization(.never)
+            }
+            .background(.clear)
+            .padding(.trailing, 16)
+            .padding(.leading, 20)
+            .padding(.vertical, 4)
+
+    }
+}
+
+//
+//
+//
+//
+//struct FolderDecorateView: View {
+//    @State var name: String = ""
+//    @State private var textFieldString: String = ""
+//    var body: some View {
+//        VStack {
+//            
+//            HStack {
+//                Text("폴더 꾸미기")
+//                    .font(.pretendard(size: 20, family: .bold))
+//                    .foregroundStyle(.black)
+//                    .padding(.leading, 20)
+//               
+//                Spacer()
+//            }
+//            
+//            HStack {
+//                
+//                TextField("폴더명을 입력해주세요", text: self.$textFieldString)
+//                    .frame(height: 45)
+//                    .textFieldStyle(PlainTextFieldStyle())
+//                    .padding([.horizontal], 4)
+//                    .cornerRadius(16)
+//                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.gray))
+//                    .padding([.horizontal], 4)
+//                    .submitLabel(.search)
+//                    .onSubmit {
+//                        //   handleSearch()
+//                    }
+//                    .onAppear {
+//                        UITextField.appearance().clearButtonMode = .whileEditing
+//                    }
+//                    .toolbar {
+//                        ToolbarItem(placement: .keyboard) {
+//                            HStack {
+//                                Spacer()
+//                            
+//                                Button(action: {
+//                                    //      hideKeyboard()
+//                                
+//                                    if textFieldString.isEmpty {
+//                                        //     isSearchViewHidden = true
+//                                        //     vm.stations = []
+//                                    }
+//                                }) {
+//                                    Image(systemName: "keyboard.chevron.compact.down")
+//                                        .foregroundColor(.blue)
+//                                }
+//                            }
+//                        }
+//                    }
+//                    .autocorrectionDisabled()
+//                    .textInputAutocapitalization(.never)
+//                }
+//                .background(.clear)
+//                .padding(.trailing, 16)
+//                .padding(.leading, 20)
+//                .padding(.vertical, 4)
+//            
+//            
+//            HStack {
+//                VStack {
+//                    Image(systemName: "camera")
+//                        .resizable()
+//                        .frame(width:36,height:36)
+//                     
+//                    Text("사진 촬영하기")
+//                }
+//                .frame(width: 170,height: 120)
+//                .overlay(
+//                RoundedRectangle(cornerRadius: 10)
+//                    .stroke(.EDEDED, lineWidth: 1)
+//                )
+//                
+//                VStack {
+//                    Image(systemName: "camera")
+//                        .resizable()
+//                        .frame(width:36,height:36)
+//                     
+//                    Text("사진 촬영하기")
+//                }
+//                .frame(width: 170,height: 120)
+//                .overlay(
+//                RoundedRectangle(cornerRadius: 10)
+//                    .stroke(.EDEDED, lineWidth: 1)
+//                )
+//            }
+//            .padding(13)
+//            Button {
+//                
+//            } label: {
+//                Text("완료")
+//                    .frame(maxWidth: .infinity)
+//                    .frame(height: 36)
+//                    .padding(.horizontal, 20)
+//            }
+//            .buttonStyle(.borderedProminent)
+//            .padding(.horizontal, 20)
+//      
+//
+//           
+//            }
+//        }
+//    }
--- modoosSubway/modoosSubway/modoosSubwayApp.swift
+++ modoosSubway/modoosSubway/modoosSubwayApp.swift
@@ -26,7 +26,7 @@ struct modoosSubwayApp: App {
 
     var body: some Scene {
         WindowGroup {
-            HomeView()
+            ModifyView()
            // FolderListView()
         }
         .modelContainer(sharedModelContainer)
