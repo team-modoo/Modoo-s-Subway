90e87aacb991f6a69221f28ec21dbb002a5f300b
LimJaeHyeon9298 <limjh9298@gmail.com>
2024-11-21 16:06:26 +0900

    [feat] 앱 로고 변경,런치스크린 시간 설정

--- modoosSubway/modoosSubway/modoosSubwayApp.swift
+++ modoosSubway/modoosSubway/modoosSubwayApp.swift
@@ -10,6 +10,7 @@ import SwiftData
 
 @main
 struct modoosSubwayApp: App {
+    @State var isSplashView = true
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
             Item.self,
@@ -27,9 +28,53 @@ struct modoosSubwayApp: App {
 
     var body: some Scene {
         WindowGroup {
+            if isSplashView {
+                LaunchScreenView()
+                    .ignoresSafeArea()
+                    .onAppear {
+                        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3) {
+                            isSplashView = false
+                        }
+                    }
+                   
+            } else {
+                HomeView()
+            }
             //SettingView()
-           ModifyFolderView()
+         
         }
         .modelContainer(sharedModelContainer)
+    }
+}
+
+
+
+
+//@main
+//struct CampairApp: App {
+//    @State var isSplashView = true
+//    var body: some Scene {
+//        WindowGroup {
+//            if isSplashView {
+//                LaunchScreenView()
+//                    .ignoresSafeArea()
+//                    .onAppear {
+//                        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) {
+//                            isSplashView = false
+//                        }
+//                    }
+//            } else {
+//                TabbarView()
+//            }
+//        }
+//    }
+//}
+
+struct LaunchScreenView: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> some UIViewController {
+        let controller = UIStoryboard(name: "Launch Screen", bundle: nil).instantiateInitialViewController()!
+        return controller
+    }
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
     }
 }
